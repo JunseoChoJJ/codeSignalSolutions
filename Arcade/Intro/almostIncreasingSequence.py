1d2d287cb055a57cacf628461f92f4cb6a0dd205
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2023-02-08 19:46:47 -0500

    Create almostIncreasingSequence.py

---
+++ Arcade/Intro/almostIncreasingSequence.py
@@ -0,0 +1,18 @@
+def solution(sequence):
+    droppped = False
+    last = prev = min(sequence) - 1
+    print(last)
+    
+    for elm in sequence:
+        if elm <= last:
+            if droppped:
+                return False
+            else:
+                droppped = True
+            if elm <= prev:
+                prev = last
+            elif elm >= prev:
+                prev = last = elm
+        else:
+            prev, last = last, elm
+    return True
