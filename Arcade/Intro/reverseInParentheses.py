ffd5b120dbf5dfe8f1162fcd6abefe3339926e5a
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2023-02-09 16:00:51 -0500

    Create reverseInParentheses.py

---
+++ Arcade/Intro/reverseInParentheses.py
@@ -0,0 +1,9 @@
+def solution(s):
+    # try again
+    for i in range(len(s)):
+        if s[i] == "(":
+            start = i
+        if s[i] == ")":
+            end = i
+            return solution(s[:start] + s[start+1:end][::-1] + s[end+1:])
+    return s
