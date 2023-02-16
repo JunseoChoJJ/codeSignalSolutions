1954f820b00d09128479f6d9503572f516b76bb5
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2023-02-16 09:20:07 -0500

    Create isDigit.py

---
+++ Arcade/Intro/isDigit.py
@@ -0,0 +1,12 @@
+def solution(symbol):
+    if symbol.isdigit():
+        return True
+    else:
+        return False
+
+
+    '''
+    best voted solution
+    return symbol.isdigit()
+    
+    I am using python I need to be more simple like best voted solution
