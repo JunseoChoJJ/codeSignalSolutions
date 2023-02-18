60aa6857a71cacd522287e2a415edecde467d3b9
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2023-02-17 22:04:14 -0500

    Update add_two_digit.py

--- Arcade/Core/add_two_digit.py
+++ Arcade/Core/add_two_digit.py
@@ -1,1 +1,14 @@
-
+def solution(n):
+    ans = []
+    
+    ans.append(int(str(n)[0])) 
+    ans.append(int(str(n)[1]))
+    return sum(ans)
+    
+    
+    '''
+    best voted solution
+    return n // 10 + n % 10
+    
+    I don't need to use append function I rather just use math 
+    '''
