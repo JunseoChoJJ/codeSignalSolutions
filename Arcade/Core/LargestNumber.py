8ec42bfefad0502ec1573f848932e4f119a00cb9
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2023-02-17 22:04:55 -0500

    Create LargestNumber.py

---
+++ Arcade/Core/LargestNumber.py
@@ -0,0 +1,18 @@
+def solution(n):
+    
+    ans = ""
+    for i in range(n):
+        ans += '9'
+        
+    return int(ans)
+
+
+    '''
+    best voted solution
+    return int('9'*n)
+    
+    this is way better in time perspective
+    
+    I need to think if there is another way not using for or while loop
+    
+    '''
