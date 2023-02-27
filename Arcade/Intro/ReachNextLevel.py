767d0f0cae699e440bec403e74b8ca42741a95f2
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2023-02-26 23:49:48 -0500

    Create ReachNextLevel.py

---
+++ Arcade/Intro/ReachNextLevel.py
@@ -0,0 +1,15 @@
+def solution(experience, threshold, reward):
+    if experience + reward >= threshold:
+        return True
+    else:
+        return False
+    
+    
+    '''
+    best voted solution
+    
+    return experience + reward >= threshold
+    
+    I can do this
+    
+    '''
