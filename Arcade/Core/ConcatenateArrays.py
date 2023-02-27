d7a1dfcd4aed884485558db296fedb0bc8d540cc
Junseo Cho <114173022+JunseoChoJJ@users.noreply.github.com>
2023-02-26 23:53:45 -0500

    Create ConcatenateArrays.py

---
+++ Arcade/Core/ConcatenateArrays.py
@@ -0,0 +1,16 @@
+def solution(a, b):
+    for i in range(len(b)):
+        a.append(b[i])
+    
+    return a
+    
+    '''
+    best voted solution
+    
+    return a + b
+
+    
+    what I've learn
+    - learn powerful python function a + b you can add list  
+    - you cannot do a = a.append(b[i]) <- it makes sense that you don't need to do it. 
+    '''
